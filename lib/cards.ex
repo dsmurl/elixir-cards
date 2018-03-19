2039e6eefc3fd8a57ff4eb520de1628811374b61
sam bagot <dsmurl@gmail.com>
2018-03-18 23:49:38 -0500

    some docs and tests.

--- lib/cards.ex
+++ lib/cards.ex
@@ -3,6 +3,25 @@ defmodule Cards do
   Documentation for Cards.
   """
 
+
+
+
+  @doc """
+    Creates and returns a new deck.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
+      "Five of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs",
+      "Four of Clubs", "Five of Clubs", "Ace of Hearts", "Two of Hearts",
+      "Three of Hearts", "Four of Hearts", "Five of Hearts", "Ace of Diamonds",
+      "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds"]
+
+  """
+
+
+
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -16,6 +35,16 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determins whether a deck contains a given card.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
@@ -25,7 +54,7 @@ defmodule Cards do
     The `hand_size` arbument indicates how many cards should
     be in the hand.
 
-    ## Examples
+  ## Examples
 
       iex> deck = Cards.create_deck
       iex> {hand, deck} = Cards.deal(deck, 1)
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,16 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-#  test "greets the world" do
-#    assert Cards.hello() == :world
-#  end
+  test "create_deck makes 20 cards" do
+    deck = Cards.create_deck()
+    deck_length = length(deck)
+    assert deck_length == 20
+    assert Cards.contains?(deck, "Ace of Spades") == true
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck()
+    refute deck == Cards.shuffle(deck)
+  end
+
 end
